017e1f710011882673a77f25f515d6e1c6b777d5
ryanhamley <rhamley@skycatch.com>
2016-10-30 21:08:58 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <mapbox></mapbox>
+  </div>
+</template>
+
+<script>
+import Mapbox from './components/Mapbox.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Mapbox
+  }
+}
+</script>
---
+++ src/components/Mapbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="map">
+  </div>
+</template>
+
+<script>
+const mapboxgl = require('mapbox-gl')
+mapboxgl.accessToken = 'pk.eyJ1IjoicnlhbmhhbWxleSIsImEiOiJjaWszbmluaG8wMDAzdTBrc2Q3Ymk3b3l1In0.BxdMyaYKg_0-LwANjPybNA'
+
+export default {
+  mounted() {
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/ryanhamley/ciuxgnhff00jo2irrvml8bfjk',
+        center: [-122.40995240000001, 37.7720566],
+        zoom: 13,
+        pitch: 45
+    });
+  }
+}
+</script>
+
+<style lang="scss">
+#map {
+  position:absolute;
+  top:0;
+  bottom:0;
+  width:100%;
+}
+</style>
